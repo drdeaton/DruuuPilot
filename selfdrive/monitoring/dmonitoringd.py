1c147ef8ddd5a26204968c9202138bd7aa66f7f9
firestar5683 <168790843+firestar5683@users.noreply.github.com>
2025-09-19 11:30:05 -0500

    Revert "Update dmonitoringd.py"
    This reverts commit 78e28cf82be915247c141bdca2c3ee8c1c8fdf26.

--- selfdrive/monitoring/dmonitoringd.py
+++ selfdrive/monitoring/dmonitoringd.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python3
+import gc
 
 import cereal.messaging as messaging
 from openpilot.common.params import Params
-from openpilot.common.realtime import config_realtime_process
+from openpilot.common.realtime import set_realtime_priority
 from openpilot.selfdrive.monitoring.helpers import DriverMonitoring
 
 
 def dmonitoringd_thread():
-  config_realtime_process([0, 1, 2, 3], 5)
+  gc.disable()
+  set_realtime_priority(2)
 
   params = Params()
   pm = messaging.PubMaster(['driverMonitoringState'])
