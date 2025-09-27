9b5f3ff27b63f019835e16ae6bff6e6b032006b4
firestar5683 <168790843+firestar5683@users.noreply.github.com>
2025-09-26 23:45:36 -0500

    Revert "SmState"
    This reverts commit 61336be4054a3572773985737da75fb13794eb14.

--- selfdrive/controls/radard.py
+++ selfdrive/controls/radard.py
@@ -298,8 +298,7 @@ class RadarD:
       self.tracks[ids].update(rpt[0], rpt[1], rpt[2], v_lead, rpt[3])
 
     # *** publish radarState ***
-    required_services = ['modelV2', 'carState']
-    self.radar_state_valid = sm.all_checks(required_services) and len(radar_errors) == 0
+    self.radar_state_valid = sm.all_checks() and len(radar_errors) == 0
     self.radar_state = log.RadarState.new_message()
     self.radar_state.mdMonoTime = sm.logMonoTime['modelV2']
     self.radar_state.radarErrors = list(radar_errors)
